b0600d374c07ad54feb150d772a5054ee922c2ff
weichao <17314881994@163.com>
2019-06-27 15:16:47 +0800

    是DVD分呢

--- src/main/java/TesztInfo.java
+++ src/main/java/TesztInfo.java
@@ -1,5 +1,8 @@
 public class TesztInfo {
     public static void main(String[] args) {
-        System.err.println("dgvfnfgh bfsdk dbfj hk j j");
+
+        System.err.println("123");
+        System.err.println("dgvfnfgh456");
+        System.err.println("fgvbfn ");
     }
 }
