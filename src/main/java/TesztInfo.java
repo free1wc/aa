26ac14602b84a4b395f48e85951d8279f4f16d98
weichao <17314881994@163.com>
2019-06-27 15:19:54 +0800

    测试信息

--- src/main/java/TesztInfo.java
+++ src/main/java/TesztInfo.java
@@ -1,11 +1,8 @@
 public class TesztInfo {
     public static void main(String[] args) {
-
-        System.err.println("123");
-        System.err.println("dgvfnfgh456");
-        System.err.println("fgvbfn ");
-        System.err.println("dgvfnfgh bfsdk dbfj hk j j");
-
-
+        System.err.println("1股份23");
+        System.err.println("dgvfnfgh45发给你们呢6");
+        System.err.println("fgvbfn放大功能好吧");
+        System.err.println("dgvfnfgh bfsdk dbfj hk 山东省j j");
     }
 }
